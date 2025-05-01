c8cfcf28117f76db222e098142651924f2078986
Adrigamer2950 <adrigamer2950@gmail.com>
2025-05-01 19:02:54 +0200

    Enable beta suffix

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,10 +18,10 @@ plugins {
 val versionIsBeta = (properties["version"] as String).lowercase().contains("beta")
 
 group = "me.adrigamer2950"
-version = properties["version"] as String /*+
+version = properties["version"] as String +
         if (versionIsBeta)
             "-${getGitCommitHash()}"
-        else ""*/
+        else ""
 
 repositories {
     mavenCentral()
